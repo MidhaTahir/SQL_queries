51dcf2242ca337fb4c67f2b2e3df846dec8adcba
MidhaTahir <midhatahirkhan2011@gmail.com>
2020-04-04 03:23:27 +0500

    adding more tables

---
+++ company_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE companies
+(
+	company_id VARCHAR(255),
+    company_name VARCHAR(255),
+    headquarters_phone_number INT(12),
+PRIMARY KEY (company_id)
+);
---
+++ items_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE items
+(
+	item_id VARCHAR(255),
+    item VARCHAR(255),
+    unit_price NUMERIC(10,2),
+    company_id VARCHAR(255),
+PRIMARY KEY (item_id)
+);
