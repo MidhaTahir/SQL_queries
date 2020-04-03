1e016987b2bc7be9985e2a13979581f7f81b471f
MidhaTahir <midhatahirkhan2011@gmail.com>
2020-04-04 02:57:27 +0500

    selection query

---
+++ select.sql
@@ -0,0 +1,10 @@
+SELECT * from sales;
+
+SELECT * from sales.sales;
+/*Whenever you would like to refer to an SQL object in your
+queries, you must specify the database to which it is
+applied
+1) set a default database
+2) call a table from a certain database
+database_object . sql_object
+*/
