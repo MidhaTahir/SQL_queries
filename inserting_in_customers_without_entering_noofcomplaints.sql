f814f481799d7a31b45877f5691a131fc82596f2
MidhaTahir <midhatahirkhan2011@gmail.com>
2020-04-04 17:35:45 +0500

    insert

---
+++ inserting_in_customers_without_entering_noofcomplaints.sql
@@ -0,0 +1,7 @@
+INSERT INTO customers(first_name,last_name,gender)
+VALUES ('PETER','FIGARO','M');
+
+/*
+here email is not necessary,
+as number_of_complaints by default is 0 so we havent specify it
+*/
